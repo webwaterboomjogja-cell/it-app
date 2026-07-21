b289986b51d7effc867924f6b544c34d6fee34d7
loiyen <blatukgg@gmail.com>
2026-07-21 13:38:19 +0700

    update : code

--- app/Filament/Pages/InventoryDashboard.php
+++ app/Filament/Pages/InventoryDashboard.php
@@ -17,4 +17,21 @@ class InventoryDashboard extends Page
     protected static ?int $navigationSort = 1;
 
     protected static string $view = 'filament.pages.inventory-dashboard';
+
+    public static function canAccess(): bool
+    {
+        $user = auth()->user();
+
+        if (! $user) {
+            return false;
+        }
+
+        if ($user->hasRole('super_admin')) {
+            return true;
+        }
+
+        return $user->can(
+            'page_ItScheduleCalendar'
+        );
+    }
 }
